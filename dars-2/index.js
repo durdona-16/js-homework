0481e83de7e3b24e8c9c0042c8f9a126d5957438
shohpolat <shohruxbeka@mail.ru>
2023-01-29 22:44:08 +0500

    first commit

---
+++ dars-2/index.js
@@ -0,0 +1,82 @@
+ 
+
+// prefix and postfix
+let c=1
+let d=1
+let e=++c
+console.log(e);
+// 2
+let f=d++
+console.log(f);
+// 1
+
+
+// 3 Assitment result
+let g=2
+let x=1+(g*=2)
+console.log(x);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+let a=""+1+5
+console.log(typeof a);
+// string 15
+console.log(""+1+0);
+// 10
+console.log(""-1+0);
+
+let b=""-1+0
+
+console.log(typeof b);
+// -1 number
+
+ console.log( true+true);  
+ console.log(6/"2"); 
+//  3 number
+ console.log("2"*"3");
+//  6 number
+ console.log(4+5+"px");
+//  9px string
+console.log("$"+4+5);
+// $45 string
+console.log("4"-2);
+// 2 number
+console.log("4px"-2);
+// NaN
+console.log("-9"+5);
+// -95
+console.log(null+1);
+// 1 number
+console.log(undefined+1);
+// NaN
+console.log("\t\n"-2);
+// -2
+console.log(5>4);
+// true
+console.log("apple">"pineapple");
+// false
+console.log("2">"12");
+// true
+console.log(undefined==null);
+// true
+console.log(undefined===null);
+// false
+console.log(null=="\n0\n");
+// false
+console.log(null===+"\n0\n");
+// false
+
