59a2a829ad8e20464c5a252b5b2d04e72bb96ce6
shohpolat <shohruxbeka@mail.ru>
2023-02-01 02:24:24 +0500

    dars3 homeworks

---
+++ dars3/index.js
@@ -0,0 +1,28 @@
+
+// task 1
+let a=1
+if (a=1)
+{console.log("Hello");
+}
+else{
+     console.log("goodbye");   
+}
+
+
+
+// task 2
+let b="ECMAScript"
+if(b="ECMAScript"){console.log("Right");
+}
+else{console.log("You don`t know ECMAScript")}
+
+// task 3
+let d=4
+let  e=2
+ d+e<4? console.log ("Below"):console.log("over")
+
+
+
+  
+
+
