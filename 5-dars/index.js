724ccb98850afee90769dab4681f27d6324d0b80
shohpolat <shohruxbeka@mail.ru>
2023-02-06 00:40:27 +0500

    func homework

---
+++ 5-dars/index.js
@@ -0,0 +1,22 @@
+// function confirmage(age){
+
+// if(age>=18){
+//     console.log("Ruxsat etildi");
+// }
+// else{
+//     console.log("18 dan kichik-ruxsat etilmasin");
+// }
+
+// }
+// confirmage(18)
+
+
+
+
+function confirmage(age){
+
+    age > 18 ? console.log("Ruxsat etildi") : console.log("18 dan kichik-ruxsat etilmasin")
+    
+}
+
+confirmage(12)
