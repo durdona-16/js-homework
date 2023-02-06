c4b9c8066681abf78bb962de2b50f2e6716538e9
shohpolat <shohruxbeka@mail.ru>
2023-02-06 08:09:02 +0500

    multiply function

--- 5-dars/index.js
+++ 5-dars/index.js
@@ -12,11 +12,31 @@
 
 
 
+// ion confirmage(age){
 
-function confirmage(age){
-
-    age > 18 ? console.log("Ruxsat etildi") : console.log("18 dan kichik-ruxsat etilmasin")
+//     age > 18 ? console.log("Ruxsat etildi") : console.log("18 dan kichik-ruxsat etilmasin")
     
+// }
+
+// confirmage(12)
+// funct
+
+
+
+
+function pow(num1,num2){
+
+
+    let a=1
+    for(i=1;i<=num2;i++){
+
+      a = a*num1
+
+    }
+
+    console.log(a)
+
 }
 
-confirmage(12)
+
+pow(3,6)
